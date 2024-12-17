9c65ca037df6ba77a8b71b343f858d74863ef38c
Santi-Lp <santiagolopriore@gmail.com>
2024-12-17 16:06:56 -0300

    Version Final

--- src/config/enviroment.js
+++ src/config/enviroment.js
@@ -8,7 +8,8 @@ const ENVIROMENT= {
     JWT_SECRET: process.env.JWT_SECRET,
     EMAIL_PASS: process.env.EMAIL_PASS,
     EMAIL_USER: process.env.EMAIL_USER,
-    API_KEY_INTERN: process.env.API_KEY_INTERN
+    API_KEY_INTERN: process.env.API_KEY_INTERN,
+    URL_FRONT: process.env.URL_FRONT
 }
 
 export default ENVIROMENT
